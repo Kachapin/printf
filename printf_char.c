80e6a8b049b05cd229d3bfee5b05e7b67aae2b49
Kachapin <kevinkachapin@gmail.com>
2022-10-21 01:30:12 +0300

    More changes and file additions

--- printf_char.c
+++ printf_char.c
@@ -1,5 +1,15 @@
-<<<<<<< HEAD
-bxfb
-=======
-#include main.h
->>>>>>> dc9732e9dcaabd265533a3c5ea9fb7684548b62c
+#include "main.h"
+
+/**
+ * printf_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int printf_char(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
+}
--- printf_hex.c
+++ printf_hex.c
@@ -1,5 +1,37 @@
-<<<<<<< HEAD
-fnnnf
-=======
-#include main.h
->>>>>>> dc9732e9dcaabd265533a3c5ea9fb7684548b62c
+#include "main.h"
+
+/**
+ * printf_hex - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_hex(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
